558684e249816d48c416901db636918fd5f8af91
hfdzrmdhn <rmuhamadhafids@gmail.com>
2021-10-23 05:29:06 +0700

    One

---
+++ app/src/main/java/com/bottnav/id/FragmentC1.java
@@ -0,0 +1,16 @@
+package com.bottnav.id;
+import androidx.fragment.app.Fragment;
+import android.os.Bundle;
+import android.view.ViewGroup;
+import android.view.LayoutInflater;
+import android.view.View;
+
+public class FragmentC1 extends Fragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		return inflater.inflate(R.layout.fragment_c1 , container , false);
+	}
+    
+    
+}    
---
+++ app/src/main/java/com/bottnav/id/MainActivity.java
@@ -0,0 +1,68 @@
+package com.bottnav.id;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import android.view.MenuItem;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.widget.TextView;
+import androidx.appcompat.app.ActionBar;
+
+public class MainActivity extends AppCompatActivity {
+	
+	private Toolbar toolbar;
+	private ActionBar toolbarTitle;
+	private BottomNavigationView bottomNav;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		toolbar = findViewById (R.id.toolbar);
+		setSupportActionBar(toolbar);
+		toolbarTitle = getSupportActionBar();
+		
+		bottomNav = findViewById (R.id.bottom_navigation);
+		getSupportFragmentManager().beginTransaction().replace(R.id.frame_container , new FragmentC1()).commit();
+		bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener(){
+			@Override
+			public boolean onNavigationItemSelected(MenuItem item){
+				Fragment selectedFragment = null;
+				switch(item.getItemId()){
+					case R.id.mHome:
+						selectedFragment = new FragmentC1();
+						toolbarTitle.setTitle("Bottom Navigation Fragment");
+						break;
+					case R.id.mProduk:
+						selectedFragment = new FragmentC2();
+						toolbarTitle.setTitle("Produk");
+						break;
+					case R.id.mChat:
+						selectedFragment = new FragmentC3();
+						toolbarTitle.setTitle("Chat");
+						break;
+					case R.id.mPenjualan:
+						selectedFragment = new FragmentC4();
+						toolbarTitle.setTitle("Penjualan");
+						break;
+					case R.id.mLainnya:
+						selectedFragment = new FragmentC5();
+						toolbarTitle.setTitle("Lainnya");
+						break;
+					
+				}
+				getSupportFragmentManager().beginTransaction().replace(R.id.frame_container , selectedFragment).commit();
+				return true;
+			}
+		});
+	}
+	
+}
+
